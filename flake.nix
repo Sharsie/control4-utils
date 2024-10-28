68acccb889ed676ddf2ed9111433a2dbfcf70b07
Arnie <i@cechis.cz>
2024-10-28 16:12:06 +0100

    Update flakes

--- flake.nix
+++ flake.nix
@@ -8,63 +8,121 @@
     {
       formatter = nix.formatter;
 
+      packages = nix.lib.forAllSystems (
+        pkgs:
+        let
+          version = "rev-${self.shortRev or self.dirtyShortRev}";
+        in
+        {
+          devenv-up = self.devShells.${pkgs.system}.default.config.procfileScript;
+          devenv-test = self.devShells.${pkgs.system}.default.config.test;
+          driver-manager = pkgs.buildGoModule {
+            inherit version;
+
+            pname = "driver-manager";
+
+            CGO_ENABLED = 0;
+
+            src = ./.;
+            subPackages = [ "tools/driver-manager" ];
+
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.version=${version}"
+            ];
+
+            vendorHash = null;
+          };
+        }
+      );
+
       devShells = nix.lib.forAllSystems (pkgs: {
-        default = pkgs.devshell.mkShell {
-          name = "control4-library";
+        default = nix.lib.mkDevenvShell {
+          inherit pkgs;
 
-          packages = [
-            pkgs.go
-            nix.lib.control4_env.${pkgs.system}
-          ];
+          inputs = {
+            self = self;
+            nixpkgs = pkgs;
+          };
 
-          commands = [
+          modules = [
             {
-              name = "lint";
-              help = "run linters";
-              command = ''
-                ${nix.lib.cd_root}
-                ${pkgs.golangci-lint}/bin/golangci-lint run --sort-results --out-format tab --config ${nix.lib.golangci-config-file} ./...
-              '';
-            }
-            {
-              name = "fix";
-              help = "format & fix found issues";
-              command = ''
-                ${nix.lib.cd_root}
-                nix fmt .
-                ${pkgs.golangci-lint}/bin/golangci-lint run --sort-results --out-format tab --config ${nix.lib.golangci-config-file} --fix --issues-exit-code 0 ./...
-              '';
-            }
-            {
-              name = "generate";
-              command = ''
-                ${nix.lib.cd_root}
-                rm -rf ./src/knx/gen
-                go run ./tools/knx-group-address-generator ./knx_group_addresses.csv ./src/knx/gen
-              '';
-            }
-            {
-              name = "publish";
-              command = ''
-                [[ $# -eq 0 ]] && echo "Please provide a tag as the first parameter" && exit 1
-                TAG=$1
+              packages = [
+                pkgs.go
+                nix.lib.control4_env.${pkgs.system}
+              ];
 
-                OTP=''${2:-}
+              scripts = {
+                # Override golangci-lint for vscode, because the extension incorrectly assumes usage of global binaries is preferred
+                golangci-lint = {
+                  exec = ''
+                    CMD=''${1:-}
+                    if [[ "$CMD" == "run" ]]; then
+                      shift
+                      ${pkgs.golangci-lint}/bin/golangci-lint run --config ${nix.lib.golangci-config-file} $@
+                    else
+                      ${pkgs.golangci-lint}/bin/golangci-lint $@
+                    fi
+                  '';
+                };
 
-                ${nix.lib.cd_root}
+                build = {
+                  exec = ''
+                    nix build -Lv .#driver-manager
+                    cp -f result/bin/driver-manager ./bin/driver-manager-${pkgs.system}
+                  '';
+                };
 
-                [[ $(git tag -l "$TAG") ]] && echo "Tag already exists" && exit 1
+                lint = {
+                  exec = ''
+                    ${nix.lib.cd_root}
+                    ${pkgs.golangci-lint}/bin/golangci-lint run --sort-results --out-format tab --config ${nix.lib.golangci-config-file} ./...
+                  '';
+                };
 
-                npm version $TAG --allow-same-version=true --git-tag-version=false
-                git add package.json
-                git add package-lock.json
-                git commit -m "Update package to version $TAG"
-                git tag -a $TAG -m ""
-                [[ $OTP == "" ]] && npm publish --access public
-                [[ $OTP != "" ]] && npm publish --access public --otp $OTP
+                fix = {
+                  exec = ''
+                    ${nix.lib.cd_root}
+                    nix fmt ./*.nix
+                    ${pkgs.golangci-lint}/bin/golangci-lint run --sort-results --out-format tab --config ${nix.lib.golangci-config-file} --fix --issues-exit-code 0 ./...
+                  '';
+                };
 
-                echo "Please push the new git commits and tag to the remote"
-              '';
+                generate = {
+                  exec = ''
+                    ${nix.lib.cd_root}
+                    rm -rf ./src/knx/gen
+                    go run ./tools/knx-group-address-generator ./knx_group_addresses.csv ./src/knx/gen
+                  '';
+                };
+
+                publish = {
+                  exec = ''
+                    ${nix.lib.cd_root}
+                    [[ $# -eq 0 ]] && echo "Please provide a tag as the first parameter" && exit 1
+                    TAG=$1
+
+                    build
+
+                    OTP=''${2:-}
+
+                    ${nix.lib.cd_root}
+
+                    [[ $(git tag -l "$TAG") ]] && echo "Tag already exists" && exit 1
+
+                    npm version $TAG --allow-same-version=true --git-tag-version=false
+                    git add package.json
+                    git add package-lock.json
+                    git commit -m "Update package to version $TAG"
+                    git tag -a $TAG -m ""
+                    [[ $OTP == "" ]] && npm publish --access public
+                    [[ $OTP != "" ]] && npm publish --access public --otp $OTP
+
+                    echo "Please push the new git commits and tag to the remote"
+                  '';
+                };
+              };
             }
           ];
         };
